236d3429dee0f6286fe0c071979bb954ba9f6e5f
Niklas Haas <git@haasn.xyz>
2018-01-25 19:05:41 +0100

    spirv: work around glslang locale dependence
    Do this by explicitly clearing LC_NUMERIC before calling into
    libshaderc/glslang.
    
    This is not that big of a deal performance-wise since shader compilation
    is infrequent, and it also doesn't need messy global initialization
    since we can just stuff the locale_t in with the struct priv.

--- src/spirv_shaderc.c
+++ src/spirv_shaderc.c
@@ -1,10 +1,50 @@
-#include "spirv.h"
+/*
+ * This file is part of libplacebo.
+ *
+ * libplacebo is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * libplacebo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with libplacebo. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define _XOPEN_SOURCE 700
+#include <stdlib.h>
+#include <locale.h>
+
+#ifdef __APPLE__
+# include <string.h>
+# include <xlocale.h>
+#endif
+
+#ifdef __MINGW32__
+# define HAVE_WSETLOCALE 1
+# define HAVE_USELOCALE 0
+#elif defined(__GNUC__) || defined(__APPLE__)
+# define HAVE_WSETLOCALE 0
+# define HAVE_USELOCALE 1
+#else
+# error No uselocale()-like function available!
+#endif
 
 #include <shaderc/shaderc.h>
+
+#include "spirv.h"
 
 struct priv {
     shaderc_compiler_t compiler;
     shaderc_compile_options_t opts;
+
+#if HAVE_USELOCALE
+    locale_t cloc;
+#endif
 };
 
 static void shaderc_uninit(struct spirv_compiler *spirv)
@@ -12,12 +52,25 @@ static void shaderc_uninit(struct spirv_compiler *spirv)
     struct priv *p = spirv->priv;
     shaderc_compile_options_release(p->opts);
     shaderc_compiler_release(p->compiler);
+
+#if HAVE_USELOCALE
+    freelocale(p->cloc);
+#endif
+
     TA_FREEP(&spirv->priv);
 }
 
 static bool shaderc_init(struct spirv_compiler *spirv)
 {
     struct priv *p = spirv->priv = talloc_zero(spirv, struct priv);
+
+#if HAVE_USELOCALE
+    p->cloc = newlocale(LC_NUMERIC_MASK, "C", (locale_t) 0);
+    if (!p->cloc) {
+        PL_FATAL(spirv, "Failed initializing C locale?!");
+        goto error;
+    }
+#endif
 
     p->compiler = shaderc_compiler_initialize();
     if (!p->compiler)
@@ -69,6 +122,15 @@ static bool shaderc_compile(struct spirv_compiler *spirv, void *tactx,
 {
     struct priv *p = spirv->priv;
 
+    // Switch to C locale to work around glslang bugs
+#if HAVE_USELOCALE
+    locale_t oldloc = uselocale((locale_t) 0);
+    uselocale(p->cloc);
+#elif HAVE_WSETLOCALE
+    int oldcfg = _configthreadlocale(_ENABLE_PER_THREAD_LOCALE);
+    wchar_t *oldloc = _wsetlocale(LC_NUMERIC, L"C");
+#endif
+
     shaderc_compilation_result_t res = compile(p, type, glsl, false);
     int errs = shaderc_result_get_num_errors(res),
         warn = shaderc_result_get_num_warnings(res);
@@ -113,6 +175,14 @@ static bool shaderc_compile(struct spirv_compiler *spirv, void *tactx,
     }
 
     shaderc_result_release(res);
+
+#if HAVE_USELOCALE
+    uselocale(oldloc);
+#elif HAVE_WSETLOCALE
+    _wsetlocale(LC_NUMERIC, oldloc);
+    _configthreadlocale(oldcfg);
+#endif
+
     return success;
 }
 
